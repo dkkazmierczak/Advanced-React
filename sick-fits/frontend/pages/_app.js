84a91f881cf2a5e1f3db2170478f021c8e6f246c
Dominika Kaźmierczak <dkl.kazmierczak@gmail.com>
2022-03-14 15:23:22 +0100

    Changed functions

--- sick-fits/frontend/pages/_app.js
+++ sick-fits/frontend/pages/_app.js
@@ -8,10 +8,12 @@ Router.events.on('routeChangeStart', () => NProgress.start());
 Router.events.on('routeChangeComplete', () => NProgress.done());
 Router.events.on('routeChangeError', () => NProgress.done());
 
-const MyApp = ({ Component, pageProps }) => (
+export default function MyApp({ Component, pageProps }){
+  return (
   <Page>
     <Component {...pageProps} />
   </Page>
-);
+  );
+}
 
-export default MyApp;
+
--- sick-fits/frontend/pages/index.js
+++ sick-fits/frontend/pages/index.js
@@ -1,9 +1,9 @@
-const IndexPage = () => (
+export default function IndexPage(){
+  return(
   <>
     <p>Hello!</p>
     <code>var</code>
     <p>I am a child</p>
   </>
 );
-
-export default IndexPage;
+}
